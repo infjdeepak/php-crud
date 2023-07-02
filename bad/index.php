ec4dd4685790f36de2b516911dd28267a8af0930
Deepak <sumitmanni22@gmail.com>
2023-07-02 16:00:26 +0530

    added edit functionality

--- bad/index.php
+++ bad/index.php
@@ -23,6 +23,7 @@ $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Products CRUD</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -47,12 +48,12 @@ $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
           <?php foreach ($products as $i => $product) { ?>
             <tr>
               <td><?php echo $i + 1; ?></td>
-              <td><img src="<?php echo $product["image"]; ?>" alt=""></td>
+              <td><img class="small-img" src="<?php echo $product["image"]; ?>" alt=""></td>
               <td><?php echo $product["title"]; ?></td>
               <td><?php echo $product["description"]; ?></td>
               <td><?php echo $product["created_at"]; ?></td>
               <td>
-                <button class="btn btn-primary">Edit</button>
+                <a class="btn btn-primary" href="./edit.php?id=<?php echo $product["id"]; ?>">Edit</a>
                 <form action="./delete.php" method="post" class="d-inline-block">
                   <input type="hidden" name="id" value="<?php echo $product["id"]; ?>">
                   <button class="btn btn-danger">Delete</button>
@@ -68,9 +69,3 @@ $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
 </body>
 
 </html>
-
-<style>
-  td img {
-    width: 60px;
-  }
-</style>
